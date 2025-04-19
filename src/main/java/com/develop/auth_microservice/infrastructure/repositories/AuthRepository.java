90095c548a7eaf7fe3abc282b6d7eaf516cc00de
sebastian <sebastian.cifuentes0808@gmail.com>
2025-04-19 10:36:25 -0500

    Feature: JWT Validation completed

---
+++ src/main/java/com/develop/auth_microservice/infrastructure/repositories/AuthRepository.java
@@ -0,0 +1,11 @@
+package com.develop.auth_microservice.infrastructure.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.develop.auth_microservice.domain.models.Auth;
+
+import java.util.Optional;
+
+public interface AuthRepository extends JpaRepository<Auth, Long> {
+    Optional<Auth> findByEmail(String email);
+}
